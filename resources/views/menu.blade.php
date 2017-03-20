1bf7d13a78fa6710d52b4085073076552d88951b
Guigeek <geegeek21@gmail.com>
2017-03-20 17:54:10 +0100

    begining redirect menu

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,4 @@
+<form action="/game" method="get">
+    <input type="submit" value="Go to my link location"
+         name="Submit" id="frm1_submit" />
+</form>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,6 @@
 */
 
 Route::get('/', function () {
-    return view('game');
+    return view('menu');
 });
+Route::get('/game', )
