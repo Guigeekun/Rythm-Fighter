1cb56fab7ff9106d4d234d2f28a4e005e3ecd775
Unknown <raph9632@gmail.com>
2017-03-23 17:09:00 +0100

    nothings working shit is shit -Guigeek

--- resources/assets/js/game.js
+++ resources/assets/js/game.js
@@ -1,13 +1,40 @@
 
-var game = new Phaser.Game('90', '90', Phaser.AUTO, 'gameContainer', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'Rythm-Fighter', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
    game.load.image('ground', 'img/_ground/ground05.png');
+   game.load.audio('song', ['public/audio/song/minami-JudgementStar.mp3', 'public/audio/song/minami-JudgementStar.ogg']);
 }
 
+var music
+
 function create() {
-game.add.sprite(0, 0, 'ground');
+  game.stage.backgroundColor = '#182d3b';
+//  game.add.sprite(640, 370, 'ground');
+  game.input.touch.preventDefault = false;
+  music = game.add.audio('song');
+      music.play();
+  game.input.onDown.add(changeVolume, this);
+}
+function changeVolume(pointer) {
+
+    if (pointer.y < 100)
+    {
+        music.mute = false;
+    }
+    else if (pointer.y < 300)
+    {
+        music.volume += 0.1;
+    }
+    else
+    {
+        music.volume -= 0.1;
+    }
+
 }
 
 function update() {
 }
+function action_table()
+}
+var v1 = 0
