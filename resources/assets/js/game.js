ed8a2891cdc7fc2a406a32708a9407ba65faf8a0
Kyuminn <azerty153486@outlook.fr>
2017-04-15 12:01:55 +0200

    Added countdown

--- resources/assets/js/game.js
+++ resources/assets/js/game.js
@@ -1,8 +1,8 @@
-//Difficulté normale; 1: difficile 2: normal, 4: easy (pour un futur menu de selection.)
+//Difficultés = 1: difficile 2: normal, 4: easy (pour un futur menu de selection.)
 var difficulty = 2;
 
 //On fabrique un tableau contenant des objets. Chaque objet est la caractéristique d'une musique avec son titre, son path vers son ogg/mp3 et son tempo/s
-var songs = [{title:"Judgement Star", pathMp3:'/audio/song/minami-JudgementStar.mp3', pathOgg:'/audio/song/minami-JudgementStar.ogg', tempo:1/(190/60)*difficulty}];
+var songs = [{title:"Judgement Star", pathMp3:'/audio/song/minami-JudgementStar.mp3', pathOgg:'/audio/song/minami-JudgementStar.ogg', tempo:1/(190.04/60)}];
 
 //Ratio en 16:9
 var gameWidth = 1280*window.innerWidth/1280;
@@ -16,7 +16,7 @@ var music;
 //Chargement des ressources et lancement de l'écran de chargement
 function preload() {
    game.load.onFileComplete.add(loading, this);
-   text = game.add.text(gameWidth/3, gameHeight/2, 'Loading...', { fill: '#ffffff' });
+   text = game.add.text(gameWidth/3, gameHeight/2, 'Loading...', { fill:'#ffffff', size:"20" });
    document.getElementById("rythmFighterBg").style.height = gameHeight;
    document.getElementById("rythmFighterBg").style.width = gameWidth;
    game.load.image('player1', 'img/_rocky/rocky01.png');
@@ -33,13 +33,7 @@ function loading(progress, cacheKey, success, totalLoaded, totalFiles){
 }
 
 function create() {
-  //Delete loading screen (loading bar and text)
-  text.destroy();
-  $(".meter").remove();
-
-  //load background and img
-  player1 = game.add.sprite(gameWidth/10, gameHeight/1.9, 'player1');
-  player2 = game.add.sprite(gameWidth/1.25, gameHeight/1.9, 'player2');
+  text.setText("Loading...");
 
   //Create player 1 & 2 with inputs
   p1 = new player(Phaser.Keyboard.Q, Phaser.Keyboard.S, Phaser.Keyboard.D, Phaser.Keyboard.Z);
@@ -56,17 +50,30 @@ function create() {
 
 }
 
-function counter() {
-  p1.counter();
-  p2.counter();
-}
-
 //startGame se déclenche quand la musique est décodé par le navigateur
 function startGame(){
-  music.play();
-  game.time.events.loop(Phaser.Timer.SECOND*songs[0].tempo, beatLoop, this);
+  //Delete loading screen (loading bar and text)
+  $(".meter").remove();
+
+  //load background and img
+  player1 = game.add.sprite(gameWidth/10, gameHeight/1.9, 'player1');
+  player2 = game.add.sprite(gameWidth/1.25, gameHeight/1.9, 'player2');
+
+  //Start music and timer
+  countdown = 4;
+  game.time.events.repeat(1000, 4, function(){text.setText(countdown -= 1);}, this);
+  game.time.events.onComplete.add(function(){
+    text.destroy();
+    music.play();
+    game.time.events.loop(Phaser.Timer.SECOND*songs[0].tempo*difficulty, beatLoop, this);
+  });
 }
 //Action à executer à chaque boucle de temps (change en fonction de la variable difficulty)
 function beatLoop(){
     console.log("Test"); //Pour test, un message est affiché en boucle dans la console
+}
+
+function counter() {
+  p1.counter();
+  p2.counter();
 }
