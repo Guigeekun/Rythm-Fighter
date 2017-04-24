d04d05b322e83b5e8dd852c6f3d7ee15d6362a84
Unknown <raph9632@gmail.com>
2017-04-24 11:34:13 +0200

    edit page info

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -3,7 +3,24 @@
 @section('title', 'Rythm Fighter')
 
 @section('body')
-<img id="rythmFighterBg" src="./img/Background.png"></img>
+<div align="center">
+<img id="rythmFighterBg" src="images/Background.png"></img>
+<FONT size="30" color="#FFFFFF" face="arial">
+<h2><u>voici les regles</h2></u>
+<h1><u>un jeu de combat</u></h1>
+<P> ce jeu a été pensé comme un jeu de combat :c' est a dire que le but du jeu est de faire chuter les points de vies de l'adversaire a 0<p>
+<p>vous avez le choix entre plusieurs types d'attaques :
+  <p>la petite attaque
+  <p>la grande attaque
+  <p>la guard
+  <p> et la guard break
+    <h1><u>u jeu basé sur le principe du pierre feuille ciseaux </h1></u>
+ <p>meme s'il reste plus compliqué (heureusement) le principe reste le meme.
+  <p>chaques attaques representent pour d'autres leurs faiblesses
+    <p>voici le tableau de chaques faiblesses :
+      <table style="width:100%"> 
+</font>
+
 @stop
 @section('scripts')
 <script>
@@ -11,5 +28,8 @@ $(function(){
   $('#rythmFighterBg').css('width', $(window).width())
                   .css('height', $(window).height());
 });
+
+
+
 </script>
 @stop
